158d9ee3fb9dd6c492f62c2e7b194959eb1994be
Dm <630372254@qq.com>
2017-06-06 14:42:37 +0800

    quick_learn

--- learn_matplotlib/table_matplotlib.py
+++ learn_matplotlib/table_matplotlib.py
@@ -8,4 +8,3 @@ area = np.pi * (15 * np.random.rand(N))**2 # 0 to 15 point radiuses
 color = 2 * np.pi * np.random.rand(N)
 plt.scatter(x, y, s=area, c=color, alpha=0.5, cmap=plt.cm.hsv)
 plt.show()
-
